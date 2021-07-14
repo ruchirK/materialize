bbab90a6d326c379fff045a0bd60a259b7b33867
Ruchir Khaitan <ruchir0001@gmail.com>
2021-07-14 16:59:39 -0400

    persist: do not merge: add a prototype compact function
    This commit introduces a prototype compaction function which attempts to compact a
    trace up to a `as_of` time, but instead of compacting all updates <= as_of, it merely
    finds the subrange of batches containing times < as_of and compacts them into a single
    batch and forwards their updates to `max(batch.upper()) - 1`.
    
    Future work:
    
    * Actually compact up to as_of (this requires potentially splitting a batch in two)
    * Push the computationally intensive work of reading batches and consolidating them onto
      a separate thread
    * Figure out a better interface between the Indexed, Trace, and Cache layers (right now we
      rediscover the Id for the Trace by looking inside a previously persisted batch)

--- src/persist/src/indexed/trace.rs
+++ src/persist/src/indexed/trace.rs
@@ -18,6 +18,7 @@ use std::sync::Arc;
 use differential_dataflow::lattice::Lattice;
 use differential_dataflow::trace::Description;
 use timely::progress::{Antichain, Timestamp};
+use timely::PartialOrder;
 
 use crate::error::Error;
 use crate::indexed::cache::BlobCache;
@@ -115,6 +116,87 @@ impl<K: Data, V: Data> BlobTrace<K, V> {
         Ok(())
     }
 
+    /// Compact the subrange of batches containing times < as_of to a single batch from
+    /// [0, max(batch_upper <= as_of)) and forward all updates in that batch to max(batch_upper <= as_of) - 1
+    ///
+    /// In general we would want to compact all updates <= as_of into a single batch and split the batch containing updates
+    /// [lo, hi) s.t. lo <= as_of < hi into two batches [lo, as_of + 1), [as_of + 1, hi) and then do the compaction aligned to
+    /// a batch boundary but this was a simpler place to start.
+    pub fn compact<L: Blob>(
+        &mut self,
+        as_of: Antichain<u64>,
+        blob: &mut BlobCache<K, V, L>,
+    ) -> Result<(), Error> {
+        if PartialOrder::less_equal(&self.ts_upper(), &as_of) {
+            return Err(Error::from(format!("invalid compaction")));
+        }
+
+        // TODO
+        // check the lower and since as well
+
+        // Need to pull out all of the batches with upper <= as_of
+        let mut to_compact = vec![];
+        for (desc, key) in self.batches.iter() {
+            if PartialOrder::less_equal(desc.upper(), &as_of) {
+                to_compact.push((desc, key));
+            }
+        }
+
+        if to_compact.is_empty() {
+            return Ok(());
+        }
+
+        // Now we need to extract all of the records from all of those batches.
+        let mut updates = vec![];
+
+        for (_, key) in to_compact.iter() {
+            updates.push(blob.get_trace_batch(key)?);
+        }
+
+        let mut buf = vec![];
+
+        for batch in updates.iter() {
+            buf.extend(batch.updates.iter().cloned());
+        }
+
+        // Get lower and upper bounds from the batches we just looked at
+        let lower = updates.first().expect("known to exist").desc.lower();
+        let upper = updates.last().expect("known to exist").desc.upper();
+        // We know since we don't allow empty batches that lower has to be
+        // strictly less than upper.
+        debug_assert!(PartialOrder::less_than(lower, upper));
+        let as_of_time = upper[0].saturating_sub(1);
+
+        // Forward the times of those updates to upper
+        for ((_, _), t, _) in buf.iter_mut() {
+            *t = as_of_time;
+        }
+
+        // Consolidate
+        differential_dataflow::consolidation::consolidate_updates(&mut buf);
+
+        // Construct a new BlobTraceBatch
+        let desc = Description::new(
+            lower.clone(),
+            upper.clone(),
+            Antichain::from_elem(as_of_time),
+        );
+        let new_batch = BlobTraceBatch {
+            desc: desc.clone(),
+            updates: buf,
+        };
+
+        let key = self.new_blob_key();
+        blob.set_trace_batch(key.clone(), new_batch)?;
+        self.batches
+            .retain(|(desc, _)| !PartialOrder::less_equal(desc.upper(), &as_of));
+        self.batches.insert(0, (desc, key));
+
+        // TODO actually clear the unwanted batches from the blob storage
+
+        Ok(())
+    }
+
     /// Returns a consistent read of all the updates contained in this trace.
     pub fn snapshot<L: Blob>(
         &self,
@@ -144,5 +226,66 @@ impl<K: Clone, V: Clone> Snapshot<K, V> for TraceSnapshot<K, V> {
             return true;
         }
         false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::indexed::encoding::Id;
+    use crate::indexed::SnapshotExt;
+    use crate::mem::MemBlob;
+
+    use super::*;
+
+    #[test]
+    fn trace_compact() -> Result<(), Error> {
+        let mut blob = BlobCache::new(MemBlob::new("trace_compact")?);
+        let mut t = BlobTrace::new(BlobTraceMeta::new(Id(0)));
+
+        let batch = BlobTraceBatch {
+            desc: Description::new(
+                Antichain::from_elem(0),
+                Antichain::from_elem(1),
+                Antichain::from_elem(0),
+            ),
+            updates: vec![(("k".to_string(), "v".to_string()), 0, 1)],
+        };
+
+        assert_eq!(t.append(batch, &mut blob), Ok(()));
+        let batch = BlobTraceBatch {
+            desc: Description::new(
+                Antichain::from_elem(1),
+                Antichain::from_elem(3),
+                Antichain::from_elem(0),
+            ),
+            updates: vec![(("k".to_string(), "v".to_string()), 2, 1)],
+        };
+        assert_eq!(t.append(batch, &mut blob), Ok(()));
+
+        let batch = BlobTraceBatch {
+            desc: Description::new(
+                Antichain::from_elem(3),
+                Antichain::from_elem(9),
+                Antichain::from_elem(0),
+            ),
+            updates: vec![(("k".to_string(), "v".to_string()), 5, 1)],
+        };
+        assert_eq!(t.append(batch, &mut blob), Ok(()));
+
+        t.compact(Antichain::from_elem(3), &mut blob)?;
+
+        let snapshot = t.snapshot(&blob)?;
+
+        let updates = snapshot.read_to_end();
+
+        assert_eq!(
+            updates,
+            vec![
+                (("k".to_string(), "v".to_string()), 2, 2),
+                (("k".to_string(), "v".to_string()), 5, 1)
+            ]
+        );
+
+        Ok(())
     }
 }
